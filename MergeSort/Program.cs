debb0499788702836e4f3b2164c9b3abba88534f
Guilherme Machado <guilherme_henriqueoh@live.com>
2023-09-21 19:32:27 -0300

    Código V1.0, contém todos os métodos de ordenação mas ainda não utiliza os demais métodos para fazer a mesclagem do MergeSort

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,2 +1,187 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static Random random = new Random();
+
+    static void Main(string[] args)
+    {
+        int[] sizes = { 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 5000000 };
+
+        foreach (int size in sizes)
+        {
+            int[] arr = GenerateRandomArray(size);
+            int[] temp = new int[size];
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine($"Tamanho do vetor: {size}");
+
+            // Merge Sort
+            Array.Copy(arr, temp, size); // Copie o array original para evitar efeitos colaterais
+            stopwatch.Start();
+            MergeSort(temp, 0, size - 1);
+            stopwatch.Stop();
+            Console.WriteLine($"Merge Sort: Tempo de execução {stopwatch.ElapsedMilliseconds}ms");
+
+            // Bubble Sort
+            Array.Copy(arr, temp, size);
+            stopwatch.Reset();
+            stopwatch.Start();
+            BubbleSort(temp);
+            stopwatch.Stop();
+            Console.WriteLine($"Bubble Sort: Tempo de execução {stopwatch.ElapsedMilliseconds}ms");
+
+            // Selection Sort
+            Array.Copy(arr, temp, size);
+            stopwatch.Reset();
+            stopwatch.Start();
+            SelectionSort(temp);
+            stopwatch.Stop();
+            Console.WriteLine($"Selection Sort: Tempo de execução {stopwatch.ElapsedMilliseconds}ms");
+
+            // Insertion Sort
+            Array.Copy(arr, temp, size);
+            stopwatch.Reset();
+            stopwatch.Start();
+            InsertionSort(temp);
+            stopwatch.Stop();
+            Console.WriteLine($"Insertion Sort: Tempo de execução {stopwatch.ElapsedMilliseconds}ms");
+
+            Console.WriteLine();
+        }
+    }
+
+    static int[] GenerateRandomArray(int size)
+    {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = random.Next(1000000); // Gere números aleatórios no intervalo de 0 a 999999
+        }
+        return arr;
+    }
+
+    static void Merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        Array.Copy(arr, left, leftArr, 0, n1);
+        Array.Copy(arr, middle + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2)
+        {
+            if (leftArr[i] <= rightArr[j])
+            {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+            MergeSort(arr, left, middle);
+            MergeSort(arr, middle + 1, right);
+            Merge(arr, left, middle, right);
+        }
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+
+    static void SelectionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+
+    static void InsertionSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}
