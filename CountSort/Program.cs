0bf3d7f2f3324ac1e093c73d1ad2498c91a9de89
Guilherme Machado <guilherme_henriqueoh@live.com>
2023-10-05 19:51:21 -0300

    Criando a função de ordenação correta

--- CountSort/Program.cs
+++ CountSort/Program.cs
@@ -3,31 +3,48 @@ using System.Diagnostics;
 
 class Program
 {
-
-
-    static void Main(string[] args)
+    static void CountSort(int[] arr)
     {
-        int[] tamanhos = { 500, 1000, 5000, 10000, 50000, 100000, 5000000 };
+        int[] countingArray = new int[75]; // Array de contagem com 75 elementos
 
-        foreach (int tamanho in tamanhos)
+        for (int i = 0; i < arr.Length; i++)
         {
-            int[] vetor = new int[tamanho];
-            Random random = new Random();
+            countingArray[arr[i] - 1]++; // Incrementa a contagem para o valor atual - 1
+        }
 
-            for (int i = 0; i < tamanho; i++)
+        int index = 0;
+        for (int i = 0; i < countingArray.Length; i++)
+        {
+            while (countingArray[i] > 0)
             {
-                vetor[i] = random.Next(1, 76); // Números entre 1 e 75
+                arr[index] = i + 1; // Define o valor no índice apropriado
+                index++;
+                countingArray[i]--;
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Random random = new Random();
+
+        int[] sizes = { 500, 1000, 5000, 10000, 50000, 100000, 5000000 };
+
+        foreach (int size in sizes)
+        {
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = random.Next(1, 76); // Preenche o vetor com números aleatórios entre 1 e 75
             }
 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            int[] sortedVetor = CountSort(vetor);
+            CountSort(arr);
 
             stopwatch.Stop();
-            TimeSpan tempoExecucao = stopwatch.Elapsed;
-
-            Console.WriteLine($"Tamanho do vetor: {tamanho}, Tempo de execução: {tempoExecucao.TotalSeconds:F6} segundos");
+            Console.WriteLine($"Tamanho do vetor: {size}, Tempo de execução: {stopwatch.ElapsedMilliseconds} ms");
         }
     }
 }
